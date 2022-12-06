01eee1665c969d03e52d4252741838968df6b235
kamkara <lnclass.kamkara@gmail.com>
2022-12-06 13:59:23 +0000

    app/helpers/application_helper: Add app meta donnees

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+
+    #Onglet Title
+    def title
+        base_title = "Lnclass Education"
+        if @title.nil?
+        base_title
+        else
+        "#{@title} • #{base_title}"
+        end
+    end
+    def app_name
+        "Lnclass Education"
+    end
+
+    def language
+        "fr"
+    end
+    #site description
+    def description
+        "Prepa Bac, Comprendre Philosophie."
+    end
+        
+    #Site Keys worlds
+    def keywords
+        "Tle, Bac, Terminale A1 Terminale A2,2nd C ..."
+    end
+    
+    #Theme color
+    def theme_color
+        "#020408"      
+    end
+        
+    def tileColor
+        "#020408"
+    end
 end
