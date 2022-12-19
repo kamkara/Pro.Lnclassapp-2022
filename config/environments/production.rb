691135aa8bcf4f0e8d3807a1ba194ffe525604de
kamkara <lnclass.kamkara@gmail.com>
2022-12-19 19:05:04 +0000

    Fix Host blocked

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts.clear
 
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
