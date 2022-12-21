3b5d38eb28399e8718ecaa3d4af4526906c7b14f
kamkara <lnclass.kamkara@gmail.com>
2022-12-21 17:42:14 +0000

    Add Error page

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module ProLnclassapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #### Custom Errors page
+    config.exceptions_app = self.routes
   end
 end
