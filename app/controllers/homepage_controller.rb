52b7805cef84be3bda59419e9c29d3f8de70f525
kamkara <lnclass.kamkara@gmail.com>
2023-01-07 13:17:38 +0000

    app/controllers/homepage_controller: Add

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,9 +3,9 @@ class HomepageController < ApplicationController
   def index
     if user_signed_in?
       @feed_materials = Material.all
-      @feed_courses   = Course.all.order('created_at asc')
+      @feed_courses   = Course.all.order('created_at ASC')
       
-    end
+    end 
     #redirect_to feed_path if user_signed_in?
     #redirect_to feed_path if user_signed_in? && set_home_feed == "feed"
     #redirect_to welcome_path if user_signed_in? && set_home_feed == "welcome"
